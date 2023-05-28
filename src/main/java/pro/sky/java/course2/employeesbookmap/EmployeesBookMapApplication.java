6fc66a13eaefdfa4405b89af4fbc26b174236743
glinka <alexandraglinka@yandex.ru>
2023-05-28 14:39:49 +0300

    initial commit

---
+++ src/main/java/pro/sky/java/course2/employeesbookmap/EmployeesBookMapApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.employeesbookmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesBookMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeesBookMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/employeesbookmap/EmployeesBookMapApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.employeesbookmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesBookMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
